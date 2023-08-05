66b7d8b7165a93190bbd47c1595679e7a112ab18
jorisdze <56741120+jorisdze@users.noreply.github.com>
2023-08-05 03:35:50 +0200

    feat: init directories

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def create_request(bucket,project_id,instance_id,access_token):
             }
         }
     result=requests.post(url, json=var,headers={'Content-Type':'application/json',
-               'Authorization': 'Bearer {}'.format(access_token)})
+               'Authorization': 'token  {}'.format(access_token)})
     print(result.text)
 
 
