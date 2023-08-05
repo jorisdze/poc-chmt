c0d160b17d78dda578c1ca6ae85a9d8f8f995c98
jorisdze <56741120+jorisdze@users.noreply.github.com>
2023-08-05 05:45:30 +0200

    feat: add statement

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def create_request(bucket,project_id,instance_id,access_token):
     dict_respons= json.loads(result.text)
     print(dict_respons["selfLink"])
     get_status = requests.get(dict_respons["selfLink"],headers={'Content-Type':'application/json',
-               'Authorization': 'Bearer {}'.format(access_token)}
+               'Authorization': 'Bearer {}'.format(access_token)})
     print(get_status.text)
 
 
