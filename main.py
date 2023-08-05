8ed8a0fb6ea6b52b88752a5ab8deab1f3aa1160b
jorisdze <56741120+jorisdze@users.noreply.github.com>
2023-08-05 02:17:20 +0200

    feat: add statement

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def list_bucket():
     storage_client = storage.Client(project="tonal-limiter-394416")
     BUCKET_NAME = "poc-chaumet"
     bucket = storage_client.get_bucket(BUCKET_NAME)
-    blobs = bucket.list_blobs()
+    blobs = bucket.list_blobs(prefix='ingest')
 
     for blob in blobs:
         print(blob.name)
