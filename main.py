8a312bc610a74d48cb8c8c1dbbce2c1e04139211
jorisdze <56741120+jorisdze@users.noreply.github.com>
2023-08-05 05:40:20 +0200

    feat: add print

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def create_request(bucket,project_id,instance_id,access_token):
 
     dict_respons= json.loads(result.text)
     print(dict_respons["selfLink"])
-    print(result.ok)
+    print(requests.get(dict_respons["selfLink"]))
 
 
 
